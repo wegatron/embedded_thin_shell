19225a3b9d9c935bc0e214de9c206d6b0923f262
wegatron <wegatron@gmail.com>
2014-07-11 18:07:39 +0800

    add colide_rigid_body_data

---
+++ test/test_collision/colide_rigid_body_data.cpp
@@ -0,0 +1,5 @@
+#include "colide_rigid_body_data.h"
+
+using namespace colide_rigid_body;
+
+void Plane::Collide (const VVec3d &nodes, const Plane &plane, const double kd, Eigen::VectorXd &u, Eigen::VectorXd &v) {}
